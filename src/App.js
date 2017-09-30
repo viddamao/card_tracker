cd3ac96e5f144444cfc7f85e760ec6e66226c769
Vidda <viddamaowj@hotmail.com>
2017-09-30 01:56:18 -0400

    added filters

--- src/App.js
+++ src/App.js
@@ -6,75 +6,35 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards:[{
-        category:"Amandine Piu",
-        id:"AP001",
-        url:"./cards/AP001.jpg",
-        name:"001"
-      },{
-        category:"Amandine Piu",
-        id:"AP002",
-        url:"./cards/AP002.jpg",
-        name:"002"
-      },{
-        category:"Amandine Piu",
-        id:"AP003",
-        url:"./cards/AP003.jpg",
-        name:"003"
-      },{
-        category:"Lali",
-        id:"LALI001",
-        url:"./cards/LALI001.jpg",
-        name:"001"
-      },{
-        category:"Lali",
-        id:"LALI002",
-        url:"./cards/LALI002.jpg",
-        name:"002"
-      },{
-        category:"Lali",
-        id:"LALI003",
-        url:"./cards/LALI003.jpg",
-        name:"003"
-      },{
-        category:"Lali",
-        id:"LALI004",
-        url:"./cards/LALI004.jpg",
-        name:"004"
-      },{
-        category:"Greetings From",
-        id:"GF001",
-        url:"./cards/GF001.jpg",
-        name:"Croatia"
-      },{
-        category:"Greetings From",
-        id:"GF002",
-        url:"./cards/GF002.jpg",
-        name:"Greece"
-      },{
-        category:"Greetings From",
-        id:"GF003",
-        url:"./cards/GF003.jpg",
-        name:"Hungary"
-      },{
-        category:"Greetings From",
-        id:"GF004",
-        url:"./cards/GF004.jpg",
-        name:"Iceland"
-      }]
-    
+      flipped:false,
+      category:"All",
+      keywords:"",
+      cards:[]
     };
 
     // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
+    this.changeCategory = this.changeCategory.bind(this);
+    this.changeKeywords = this.changeKeywords.bind(this);
   }
-
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
+  componentWillMount(){
+    var data = require('./data.json');
+    this.setState({
+      cards: data
+    });
   }
+  
 
+  changeCategory(event){
+    console.log(event);
+    this.setState({
+      category: event.target.value
+    });
+  }
+  changeKeywords(event){
+    this.setState({
+      keywords:event.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -85,21 +45,37 @@ class App extends Component {
           </div>
 
         </div>
-        <div className="Search">
-            <input type="text" name="searchbox" id="searchbox"/>
+        <div className="Filters">
+            <div>
+            <label htmlFor="category">Category</label>
+            <select name="category" id="categoryFilter" value={this.state.category} onChange={this.changeCategory}>
+              <option value="All">All</option>
+              <option value="Amandine Piu">Amandine Piu</option>
+              <option value="Lali">Lali</option>
+              <option value="Greetings From">Greetings From</option>              
+            </select>
+            </div>
+            <div>
+            <label htmlFor="searchbox">Keywords</label>
+            <input type="text" name="searchbox" id="searchbox" value={this.state.keywords} onChange={this.changeKeywords}/>
             <button>Search</button>
+            </div>
         </div>
         <ul className="Card-list" >
-          {this.state.cards.map(function(card){
-            return (
-            <li key={card.id}>
-              <img src={card.url} className="Cards" alt="cards"/>
-              <p>{card.category}</p>
-              <p>{card.name}</p>
+          {this.state.cards.map((card)=>{
+            if (this.state.category==="All"||this.state.category===card.category)
+              return (
+                <li key={card.id}>
+                  <img src={card.url} className="Cards" alt="cards"/>
+                  <p>{card.category}</p>
+                  <p>{card.name}</p>
+                </li>);
               
-            </li>);
-          })}
-            
+              else{
+                  return null;
+                }
+            })}
+              
         </ul>
       </div>
     );
