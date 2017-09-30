025564b979534aa32179c0fb6e3e7409b505b305
Vidda <viddamaowj@hotmail.com>
2017-09-30 18:03:49 -0400

    enable detail image

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ class App extends Component {
       flipped:false,
       category:"All",
       keywords:"",
-      cards:[]
+      cards:[],
+      showDetail:false,
+      currentCard:{}
     };
 
     // This binding is necessary to make `this` work in the callback
     this.changeCategory = this.changeCategory.bind(this);
     this.changeKeywords = this.changeKeywords.bind(this);
+    this.toggleImage = this.toggleImage.bind(this);
   }
   componentWillMount(){
     var data = require('./data.json');
@@ -45,9 +48,26 @@ class App extends Component {
       }
     )})
   }
+
+  toggleImage(e){
+    let current = e.target.parentNode;
+    this.setState({
+      showDetail:!this.state.showDetail,
+      currentCard:{
+        name:current.childNodes[2]?(current.childNodes[1].innerHTML+" "+current.childNodes[2].innerHTML):"",
+        url:e.target.src
+      }
+    })
+  }
   render() {
     return (
       <div className="App">
+        {this.state.showDetail?<div><div id="mask" onClick={this.toggleImage}></div>
+        <div className="Card-detail">
+          <img src={this.state.currentCard.url} alt="#" />
+          <p>{this.state.currentCard.name}</p>
+          </div></div>
+          :null}
         <div className="App-header">
           <div>
             <img src={logo} className="App-logo" alt="logo" />
@@ -73,14 +93,14 @@ class App extends Component {
 
             <p>Total of {this.state.count} cards</p>
         </div>
-        <ul className="Card-list" ref={(list)=>{this.filteredList = (list)?list:this.state.cards}}>
+        <ul className="Card-list" ref={(list)=>{this.filteredList = (list)?list:this.state.cards}} onClick={this.toggleImage}>
            { this.state.cards.map((card)=>{
             if (this.state.category==="All"||this.state.category===card.category){
               let re = new RegExp(this.state.keywords!==""?this.state.keywords:"","i");
 
               return (re.test(card.category)||re.test(card.name))? 
                 (
-                <li key={card.id}>
+                <li key={card.id} >
                   <img src={card.url} className="Cards" alt="cards"/>
                   <p>{card.category}</p>
                   <p>{card.name}</p>
